7a7996598b532c7ba82fb53ebb5a0a64a3373d1a
twiglau <twigliu@hotmail.com>
2021-12-12 23:00:10 +0800

    feat: 修改密码 退出登录

--- 代码/02-code/src/controller/user.js
+++ 代码/02-code/src/controller/user.js
@@ -16,7 +16,8 @@ const {
     registerFailInfo,
     loginFailInfo,
     deleteUserFailInfo,
-    changeInfoFailInfo
+    changeInfoFailInfo,
+    changePasswordFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 /**
@@ -139,10 +140,45 @@ async function changeInfo(ctx, { nickName,city,picture }) {
     return new ErrorModel(changeInfoFailInfo)
 
 }
+
+/**
+ * 修改密码
+ * @param {string} userName 
+ * @param {string} password 
+ * @param {string} newPassword 
+ */
+async function changePassword(userName,password,newPassword) {
+    const result = await updateUser(
+        {
+            newPassword:doCrypto(newPassword)
+        },
+        {
+            userName,
+            password:doCrypto(password)
+        }
+    )
+    if(result){
+        // 成功
+        return new SuccessModel()
+    }
+    return new ErrorModel(changePasswordFailInfo)
+}
+
+/**
+ * 退出登录
+ * @param {*} ctx 
+ * @returns 
+ */
+async function logout(ctx) {
+    delete ctx.session.userInfo
+    return new SuccessModel()
+  }
 module.exports = {
     isExist,
     register,
     login,
     deleteCurUser,
-    changeInfo
+    changeInfo,
+    changePassword,
+    logout
 }
