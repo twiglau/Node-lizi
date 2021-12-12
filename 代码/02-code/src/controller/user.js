1bbd3936cc5f84db4caf5031951f86843cdd80d6
twiglau <twigliu@hotmail.com>
2021-12-12 22:46:35 +0800

    feat: 修改基本信息 API

--- 代码/02-code/src/controller/user.js
+++ 代码/02-code/src/controller/user.js
@@ -3,14 +3,20 @@
  * @author lau
  */
 
-const { getUserInfo,createUser,deleteUser } = require('../services/user')
+const { 
+    getUserInfo,
+    createUser,
+    deleteUser,
+    updateUser
+ } = require('../services/user')
 const { ErrorModel,SuccessModel } = require('../model/ResModel')
 const {
     registerUserNameNotExistInfo,
     registerUserNameExistInfo,
     registerFailInfo,
     loginFailInfo,
-    deleteUserFailInfo
+    deleteUserFailInfo,
+    changeInfoFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 /**
@@ -96,9 +102,47 @@ async function deleteCurUser(userName){
      // 失败
      return new ErrorModel(deleteUserFailInfo)
 }
+
+/**
+ * 修改个人信息
+ * @param {Object} ctx 
+ * @param {string} nickName 
+ * @param {string} city 
+ * @param {string} picture 
+ */
+async function changeInfo(ctx, { nickName,city,picture }) {
+    const { userName } = ctx.session.userInfo
+    if(!nickName) {
+        nickName = userName
+    } 
+    // service
+    const result = await updateUser(
+        {
+            newNickName: nickName,
+            newCity: city,
+            newPicture: picture
+
+        },
+        { userName }
+    )
+    if(result) {
+        // 执行成功
+        Object.assign(ctx.session.userInfo, {
+            nickName,
+            city,
+            picture
+        })
+        // 返回
+        return new SuccessModel()
+    }
+    // 失败
+    return new ErrorModel(changeInfoFailInfo)
+
+}
 module.exports = {
     isExist,
     register,
     login,
     deleteCurUser,
+    changeInfo
 }
--- 代码/02-code/src/routes/api/user.js
+++ 代码/02-code/src/routes/api/user.js
@@ -4,7 +4,13 @@
  */
 
 const router = require('koa-router')()
-const { isExist,register,login, deleteCurUser } = require('../../controller/user')
+const { 
+    isExist,
+    register,
+    login, 
+    deleteCurUser,
+    changeInfo
+ } = require('../../controller/user')
 const userValidate = require('../../validator/user')
 const { genValidator } = require('../../middlewares/validator')
 const { loginCheck } = require('../../middlewares/loginChecks')
@@ -43,5 +49,13 @@ router.post('/delete',loginCheck, async (ctx,next) => {
         // 调用 controller
         ctx.body = await deleteCurUser(userName)
     }
+})
+
+// 修改个人信息
+router.patch('/changeInfo',loginCheck,genValidator(userValidate), async (ctx,next) => {
+    const { nickName,city,picture } = ctx.request.body
+    // controller
+    ctx.body = await changeInfo(ctx, { nickName,city,picture})
+
 })
 module.exports = router
