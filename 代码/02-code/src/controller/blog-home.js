6b0dc09244f538613d713b87063fe01626acee56
twiglau <twigliu@hotmail.com>
2021-12-13 11:07:55 +0800

    feat: 创建微博 xss 格式校验

--- 代码/02-code/src/controller/blog-home.js
+++ 代码/02-code/src/controller/blog-home.js
@@ -3,6 +3,7 @@
  * @author lau
  */
 
+const xss = require('xss')
 const { createBlogFailInfo } = require('../model/ErrorInfo')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const { createBlog } = require('../services/blog')
@@ -17,7 +18,7 @@ async function create({ userId,content,image }) {
         // 创建微博
         const blog = await createBlog({
             userId,
-            content,
+            content:xss(content),
             image
         })
         return new SuccessModel(blog)
---
+++ 代码/02-code/src/validator/blog.js
@@ -0,0 +1,31 @@
+/**
+ * @description blog 数据格式校验
+ * @author lau
+ */
+
+ const validate = require('./_validate')
+
+ // 校验规则
+ const SCHEMA = {
+   type: 'object',
+   properties: {
+       content: {
+           type: 'string'
+       },
+       image: {
+           type: 'string',
+           maxLength: 255
+       }
+   }
+ }
+ 
+ /**
+  * 校验用户数据格式
+  * @param {Object} data 用户数据
+  */
+ function blogValidate(data = {}) {
+   return validate(SCHEMA, data)
+ }
+ 
+ module.exports = blogValidate
+ 
