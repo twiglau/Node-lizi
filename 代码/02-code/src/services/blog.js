7b3812c89581e166c44ed4f41cefb241a128d708
twiglau <twigliu@hotmail.com>
2021-12-13 10:56:43 +0800

    feat: 创建微博 API

---
+++ 代码/02-code/src/services/blog.js
@@ -0,0 +1,24 @@
+/**
+ * @description 微博 service
+ * @author lau
+ */
+
+const { Blog } = require('../db/model/index')
+
+/**
+ * 创建微博
+ * @param {Object} param0 创建微博的数据 { userId, content, image }
+ */
+async function createBlog({ userId,content,image }) {
+    const result = await Blog.create({
+        userId,
+        content,
+        image
+    })
+    return result.dataValues
+
+}
+
+module.exports = {
+    createBlog
+}
